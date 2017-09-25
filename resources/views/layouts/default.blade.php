20cb449b495dcaf978ff198dc003476af8f9cbe4
peter <peter@enshore.nl>
2017-09-25 21:22:52 +0200

    Layout files added

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peter
+ * Date: 25-9-17
+ * Time: 21:09
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+ @include('includes.header')
+</head>
+<body>
+@include('includes.navbar')
+<div class="container-fluid">
+    @yield('content')
+
+<footer class="footer navbar-fixed-bottom">
+@include('includes.footer')
+</footer>
+</div>
+<script type="text/javascript" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" href="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
