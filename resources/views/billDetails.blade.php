b661ab6ffc5a51caeaf9907b45178b2f85f84068
peter <peter@enshore.nl>
2017-09-29 11:17:31 +0200

    +/- Added mdl styling to bills and detailbill

--- resources/views/billDetails.blade.php
+++ resources/views/billDetails.blade.php
@@ -1,30 +1,25 @@
 @extends('layouts.default')
 @section('content')
     <h3>The Bills Page</h3>
-
-    <div class="col-md-8 col-md-offset-2 ">
-
-        <table class="table">
-            <thead class="thead">
+<table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+    <thead>
+    @foreach($columns as $column)
+        <th>{{$column}}</th>
+    @endforeach
+    </thead>
+    <tbody>
+    @if($rows != NULL)
+        @foreach($rows as $row)
             <tr>
-                @foreach($columns as $column)
-                    <th>{{$column}}</th>
+                @foreach($row as $variable)
+                    <td>{{$variable}}</td>
                 @endforeach
             </tr>
-            </thead>
+        @endforeach
+    @endif
+    </tbody>
+</table>
 
-            @if($rows != NULL)
-                @foreach($rows as $row)
-                    <tr>
-                        @foreach($row as $variable)
-                            <td>{{$variable}}</td>
-                        @endforeach
-                    </tr>
-                @endforeach
-            @endif
-
-        </table>
-    </div>
     {{$warning}}
 
 @stop
--- resources/views/bills.blade.php
+++ resources/views/bills.blade.php
@@ -2,30 +2,27 @@
 @section('content')
     <h3>The Bills Page</h3>
     {{$warning}}
-
-    <div class="col-md-8 col-md-offset-2 ">
-
-        <table class="table">
-            <thead class="thead">
+<table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+    <thead>
+    <tr>
+    <tr>
+        @foreach($columns as $column)
+            <th>{{$column}}</th>
+        @endforeach
+    </tr>
+    </tr>
+    </thead>
+    <tbody>
+    @if($bills != NULL)
+        @foreach($bills as $bill)
             <tr>
-                @foreach($columns as $column)
-                    <th>{{$column}}</th>
+                @foreach($bill as $variable)
+                    <td m>{{$variable}}</td>
                 @endforeach
+                <td><a href="{{URL::to('/bills/'.$bill->id)}}" class="btn-default">details</a></td>
             </tr>
-            </thead>
-
-            @if($bills != NULL)
-                @foreach($bills as $bill)
-                    <tr>
-                        @foreach($bill as $variable)
-                            <td>{{$variable}}</td>
-                        @endforeach
-                        <td><a href="{{URL::to('/bills/'.$bill->id)}}" class="btn-default">details</a></td>
-                    </tr>
-                @endforeach
-            @endif
-
-        </table>
-    </div>
-
+        @endforeach
+    @endif
+    </tbody>
+</table>
 @stop
