6ab9049cccaefcf41d020c7e32c5904871640b57
beatusV <peterzakelijk@live.nl>
2017-10-04 14:17:06 +0200

    +/- login api call creates a token

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,7 +31,7 @@ Route::post('/profile', 'ProfileController@editUser');
 
 //API Calls
 Route::get('api/v1/users/getUser/{email}/', 'API\V1\UserApiController@getUser');
-Route::post('api/v1/users/getUser', 'API\V1\UserApiController@getLogin');
+Route::post('api/v1/users/getUser', 'API\V1\UserApiController@getLogin')->middleware('apiAuth');
 Route::post('api/v1/users/getUser/login', 'API\V1\UserApiController@login');
 Route::post('api/v1/users/editUser', 'API\V1\UserApiController@editUser');
 Route::post('api/v1/bills/getBills', 'API\V1\BillsApiController@getBills');
